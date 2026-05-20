24131949b578cbf79f9735403ef8f8b018520c75
charlesNku <charlesnkurunziza80@gmail.com>
2026-05-20 16:00:40 +0200

    Add Diagnostic Error Catcher to api/index.js

--- api/index.js
+++ api/index.js
@@ -1,3 +1,8 @@
-const { app } = require('../backend/app');
-
-module.exports = app;
+module.exports = (req, res) => {
+  try {
+    const { app } = require('../backend/app');
+    return app(req, res);
+  } catch (err) {
+    res.status(400).json({ message: `Vercel Crash Log: ${err.message}` });
+  }
+};
